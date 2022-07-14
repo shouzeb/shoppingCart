550155316cc9241a8cb5a5e3955f75df78a30454
Syed Shouzeb Hassan <38203938+shouzeb@users.noreply.github.com>
2022-07-14 11:26:12 +0500

    updated css

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+}
